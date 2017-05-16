ada4a40a75c0f5ef5a7cda43a1f31eb3eb7754ff
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2017-05-16 13:46:19 +0900

    Add nativebase setup for expo

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
+import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf')
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
